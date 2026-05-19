6e98cf6d0b70eb8d89bc4553aea9590e6bd4c77a
UzairFarooqCG <115949991+UzairFarooq1@users.noreply.github.com>
2026-05-19 19:58:52 +0300

    fix: :bug: rollback

--- emailTicket.js
+++ emailTicket.js
@@ -22,8 +22,7 @@ const loadTicketPdfModule = () => {
 
 const app = express();
 
-app.set("trust proxy", 1);
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
