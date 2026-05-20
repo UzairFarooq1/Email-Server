202eef703cc58b0919e458b94a7317526c2a2693
UzairFarooqCG <115949991+UzairFarooq1@users.noreply.github.com>
2026-05-20 06:39:03 +0300

    fix: :bug: debugging

--- emailTicket.js
+++ emailTicket.js
@@ -26,6 +26,17 @@ app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
+app.get("/", (req, res) => {
+  res.json({
+    status: "OK",
+    service: "Ticket API",
+  });
+});
+
+app.get("/health", (req, res) => {
+  res.send("healthy");
+});
+
 const storage = multer.memoryStorage();
 const upload = multer({
   storage,
